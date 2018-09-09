0fbde3a996abd07528780c9b77d5ccbdbf540f07
vasily-k <vasily-k@yandex-team.ru>
2018-09-09 22:18:57 +0300

    Implement solution and tests

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,61 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
+import kotlin.collections.ArrayList
 
-    return words.joinToString(separator = " ")
+fun findCycle(graph: List<List<Int>>, prevVertex: Int, curVertex: Int, used: BooleanArray): MutableList<Int> {
+    if (used[curVertex]) {
+        return arrayListOf(curVertex)
+    }
+    used[curVertex] = true
+    for (nextVertex in graph[curVertex]) {
+        if (nextVertex == prevVertex) {
+            continue
+        }
+        val result = findCycle(graph, curVertex, nextVertex, used)
+        if (result.isEmpty()) {
+            continue
+        }
+        if (result.size > 1 && result.first() == result.last()) {
+            return result
+        }
+        result.add(curVertex)
+        return result
+    }
+    return arrayListOf()
+}
+
+fun countDists(graph: List<List<Int>>, cycle: List<Int>): IntArray {
+    val queue = ArrayDeque<Int>()
+    val dists = IntArray(graph.size) { -1 }
+    for (vertex in cycle.dropLast(1)) {
+        queue.addLast(vertex)
+        dists[vertex] = 0
+    }
+    while (queue.isNotEmpty()) {
+        val curVertex = queue.pollFirst()
+        for (nextVertex in graph[curVertex]) {
+            if (dists[nextVertex] < 0) {
+                dists[nextVertex] = dists[curVertex] + 1
+                queue.addLast(nextVertex)
+            }
+        }
+    }
+    return dists
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    val graph = List<MutableList<Int>>(n) { ArrayList() }
+    for (i in 0 until n) {
+        val x = scanner.nextInt() - 1
+        val y = scanner.nextInt() - 1
+        graph[x].add(y)
+        graph[y].add(x)
+    }
+    for (dist in countDists(graph, findCycle(graph, 0, 0, BooleanArray(n) { false }))) {
+        print("$dist ")
+    }
 }
+
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,56 @@
 package ru.hse.spb
 
+import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class TestSource {
+
+    private val graphSimple = arrayListOf(
+            arrayListOf(1, 2),
+            arrayListOf(0, 3),
+            arrayListOf(0, 3),
+            arrayListOf(1, 2)
+    )
+
+    private val graphNormal = arrayListOf(
+            arrayListOf(1, 2),
+            arrayListOf(0, 2),
+            arrayListOf(0, 1, 3, 4),
+            arrayListOf(2, 5),
+            arrayListOf(2),
+            arrayListOf(3)
+    )
+
+    private val cycleSimple = arrayListOf(0, 2, 3, 1, 0)
+
+    private val cycleNormal = arrayListOf(0, 2, 1, 0)
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun findCycleSimpleInput() {
+        assertEquals(
+            "Cycles differ!",
+            cycleSimple,
+            findCycle(graphSimple, 0, 0, BooleanArray(graphSimple.size) { false })
+        )
+    }
+
+    @Test
+    fun findCycleNormalInput() {
+        assertEquals(
+            "Cycles differ!",
+            cycleNormal,
+            findCycle(graphNormal, 0, 0, BooleanArray(graphNormal.size) { false })
+        )
+    }
+
+    @Test
+    fun countDistsSimpleInput() {
+        assertArrayEquals("Results differ!", intArrayOf(0, 0, 0, 0), countDists(graphSimple, cycleSimple))
+    }
+
+    @Test
+    fun countDistsNormalInput() {
+        assertArrayEquals("Results differ!", intArrayOf(0, 0, 0, 1, 1, 2), countDists(graphNormal, cycleNormal))
     }
 }
