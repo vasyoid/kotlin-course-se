1dd9b7e98a939b0d8a913542c731af0442bc1ce1
vasily-k <vasily-k@yandex-team.ru>
2018-10-10 22:41:53 +0300

    Complete functionality

--- src/main/kotlin/ru/hse/spb/TexBuilder.kt
+++ src/main/kotlin/ru/hse/spb/TexBuilder.kt
@@ -10,9 +10,8 @@ interface Element {
 }
 
 @TexCommandMarker
-abstract class Tag(private val name: String) : Element {
-
-    private val children: MutableList<Element> = mutableListOf()
+abstract class Environment : Element {
+    protected val children: MutableList<Element> = mutableListOf()
 
     protected fun <T : Element> initElement(element: T, init: T.() -> Unit): T {
         element.init()
@@ -20,41 +19,131 @@ abstract class Tag(private val name: String) : Element {
         return element
     }
 
+    operator fun String.unaryPlus() {
+        children.add(TextElement(this))
+    }
+}
+
+abstract class Tag(private val name: String,
+                   private val param: String = "",
+                   private vararg val params: Pair<String, String>) : Environment() {
 
     override fun render(writer: PrintStream, indent: String) {
-        writer.println("$indent\\begin{$name}")
-            for (child in children) {
-                child.render(writer, "$indent ")
-            }
+        writer.print("$indent\\begin{$name}")
+        if (params.isNotEmpty()) {
+            writer.print(params.joinToString (", ", "[", "]") { "${it.first}=${it.second}" })
+        }
+        if (param.isNotEmpty()) {
+            writer.print("{$param}")
+        }
+        writer.println()
+        for (child in children) {
+            child.render(writer, "$indent  ")
+        }
         writer.println("$indent\\end{$name}")
     }
+
+    fun frame(title: String = "", vararg params: Pair<String, String>, init: Frame.() -> Unit) =
+        initElement(Frame(title, *params), init)
+
+    fun itemize(init: TagWithItems.() -> Unit) = initElement(TagWithItems("itemize"), init)
+
+    fun enumerate(init: TagWithItems.() -> Unit) = initElement(TagWithItems("enumerate"), init)
+
+    fun customTag(name: String, param: String = "", vararg params: Pair<String, String>, init: CustomTag.() -> Unit) =
+        initElement(CustomTag(name, param, *params), init)
+
+    fun math(init: Math.() -> Unit) = initElement(Math(), init)
+
+    fun flushleft(init: Align.() -> Unit) = initElement(Align("flushleft"), init)
+
+    fun flushright(init: Align.() -> Unit) = initElement(Align("flushright"), init)
+
+    fun center(init: Align.() -> Unit) = initElement(Align("center"), init)
 }
 
 @TexCommandMarker
 abstract class Command(
     private val name: String,
-    private val param: String,
-    private val optionalParams: Array<out String>?) : Element {
+    private val param: String = "",
+    private vararg val params: String) : Element {
 
     override fun render(writer: PrintStream, indent: String) {
-        writer.println("$indent\\$name${optionalParams?.joinToString(", ", "[", "]") ?: ""}{$param}")
+        writer.print("$indent\\$name")
+        if (params.isNotEmpty()) {
+            writer.print(params.joinToString(", ", "[", "]"))
+        }
+        if (param.isNotEmpty()) {
+            writer.print("{$param}")
+        }
+        writer.println()
     }
 }
 
-class Document : Tag("document") {
+open class Document : Tag("document") {
 
-    fun documentClass(param: String) = initElement(DocumentClass(param)) { }
+    private val headers: MutableList<Command> = mutableListOf()
 
-    fun usePackage(param: String, vararg optionalParams: String) = initElement(UsePackage(param, optionalParams)) { }
+    private fun <T : Command> initHeader(element: T, init: T.() -> Unit): T {
+        element.init()
+        headers.add(element)
+        return element
+    }
 
-    fun print(writer: PrintStream) {
-        render(writer, "")
+    override fun render(writer: PrintStream, indent: String) {
+        for (header in headers) {
+            header.render(writer, indent)
+        }
+        super.render(writer, indent)
+    }
+
+    fun documentClass(param: String) = initHeader(DocumentClass(param)) { }
+
+    fun usePackage(param: String, vararg params: String) = initHeader(UsePackage(param, *params)) { }
+
+    fun print(writer: PrintStream) = render(writer, "")
+}
+
+class TextElement(private val text: String) : Element {
+    override fun render(writer: PrintStream, indent: String) {
+        writer.println(text.trimMargin().replace(Regex("^|\\n")) { "${it.value}$indent"})
     }
 }
 
-class DocumentClass(param: String) : Command("documentclass", param, null)
-class UsePackage(param: String, optionalParams: Array<out String>?) : Command("usepackage", param, optionalParams)
+class Math : Environment() {
 
+    override fun render(writer: PrintStream, indent: String) {
+        writer.println("$indent$$")
+        for (child in children) {
+            child.render(writer, "$indent  ")
+        }
+        writer.println("$indent$$")
+    }
+}
+
+class Frame(title: String, vararg params: Pair<String, String>) : Tag("frame", title, *params)
+
+class TagWithItems(name: String) : Tag(name) {
+    fun item(label: String = "", init: TagWithItems.() -> Unit): Item {
+        val item = if (label.isNotEmpty()) {
+            initElement(Item(label)) { }
+        } else {
+            initElement(Item()) { }
+        }
+        this.init()
+        return item
+    }
+}
+
+class CustomTag(name: String, param: String, vararg params: Pair<String, String>) : Tag(name, param, *params)
+
+class Align(name: String) : Tag(name)
+
+class Item(vararg label: String) : Command("item", params = *label)
+
+class DocumentClass(className: String) : Command("documentclass", className)
+
+class UsePackage(packageName: String, vararg params: String) : Command("usepackage", packageName, *params)
 
 fun document(init: Document.() -> Unit): Document {
     val document = Document()
--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -2,10 +2,38 @@ package ru.hse.spb
 
 fun main(args: Array<String>) {
     document {
-        documentClass("beaner")
-        usePackage("babel", "russian", "cmath")
-        document {
-            usePackage("babel", "russian", "cmath")
+        documentClass("beamer")
+        usePackage("babel", "russian" /* varargs */)
+        frame("frametitle", "arg1" to "arg2") {
+            itemize {
+                for (row in (1..2)) {
+                    item { +"$row) hello" }
+                }
+            }
+            enumerate {
+                for (row in (3..4)) {
+                    item { +"$row) hello" }
+                }
+            }
+        }
+        customTag("pyglist", params = *arrayOf("language" to "kotlin")) {
+            +"""
+               |val a = 1
+               |fdsffd
+            """
+            math {
+                +"F_0 = 1,\\\\ F_1 = 1"
+                +"F_n = F_{n-1} + F_{n-2}"
+            }
+        }
+        flushleft {
+            +"left"
+            flushright {
+                +"right"
+            }
+        }
+        center {
+            +"center"
         }
     }.print(System.out)
 }
